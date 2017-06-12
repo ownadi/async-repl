2c3c21c65c06d928ab792ec37bb5f977bc043d7d
Edward Faulkner <ef@alum.mit.edu>
2017-06-12 18:17:46 -0400

    added history
    It's not shread with node's regular history, because I didn't find a userspace implementation that's compatible with node's file format.

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
 const repl = require('repl');
+const history = require('repl.history');
 const replInstance = repl.start({ prompt: 'async> ' });
 const originalEval = replInstance.eval;
 const rewrite = require('./rewrite');
+const os = require('os');
+const path = require('path');
+const historyFile = path.join(os.homedir(), '.async_repl_history');
 
+history(replInstance, historyFile);
 replInstance.eval = awaitingEval;
 
 function awaitingEval(cmd, context, filename, callback) {
